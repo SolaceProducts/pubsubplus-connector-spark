786f0aec373da69dd86d909bbb704b906a4065da
SravanThotakura05 <83568543+SravanThotakura05@users.noreply.github.com>
2024-12-17 15:58:24 +0530

    reverted queue type

--- pubsubplus-connector-spark_3.x/src/main/java/com/solacecoe/connectors/spark/streaming/solace/SolaceBroker.java
+++ pubsubplus-connector-spark_3.x/src/main/java/com/solacecoe/connectors/spark/streaming/solace/SolaceBroker.java
@@ -133,7 +133,7 @@ public class SolaceBroker implements Serializable {
             flowProp.setEndpoint(listenQueue);
             flowProp.setAckMode(JCSMPProperties.SUPPORTED_MESSAGE_ACK_CLIENT);
             EndpointProperties endpointProps = new EndpointProperties();
-            endpointProps.setAccessType(EndpointProperties.ACCESSTYPE_EXCLUSIVE);
+            endpointProps.setAccessType(EndpointProperties.ACCESSTYPE_NONEXCLUSIVE);
 
             eventListener.setBrokerInstance(this);
             FlowReceiver cons = this.session.createFlow(eventListener,
