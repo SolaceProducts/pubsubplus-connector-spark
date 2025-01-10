5abfd9a36b4544b5ea646dfe77faebd0528e4ef8
Sravan Thotakura <83568543+SravanThotakura05@users.noreply.github.com>
2025-01-10 11:21:40 +0530

    DATAGO-92181: Solace message replay support (#23)

--- pubsubplus-connector-spark_3.x/src/main/java/com/solacecoe/connectors/spark/streaming/properties/SolaceSparkStreamingProperties.java
+++ pubsubplus-connector-spark_3.x/src/main/java/com/solacecoe/connectors/spark/streaming/properties/SolaceSparkStreamingProperties.java
@@ -8,6 +8,10 @@ public class SolaceSparkStreamingProperties {
     public static final String QUEUE = "queue";
     public static final String BATCH_SIZE = "batchSize";
     public static final String BATCH_SIZE_DEFAULT = "1";
+    public static final String REPLAY_STRATEGY = "replayStrategy";
+    public static final String REPLAY_STRATEGY_REPLICATION_GROUP_MESSAGE_ID = "replayStrategyReplicationGroupMessageId";
+    public static final String REPLAY_STRATEGY_START_TIME = "replayStrategyStartTime";
+    public static final String REPLAY_STRATEGY_TIMEZONE = "replayStrategyTimeZone";
     public static final String ACK_LAST_PROCESSED_MESSAGES = "ackLastProcessedMessages";
     public static final String ACK_LAST_PROCESSED_MESSAGES_DEFAULT = "false";
     public static final String SKIP_DUPLICATES = "skipDuplicates";
