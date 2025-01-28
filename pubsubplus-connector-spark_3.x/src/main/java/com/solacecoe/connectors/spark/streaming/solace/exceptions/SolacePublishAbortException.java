d728dd12c7d10a63a0c077d4e02a965a22592e3d
SravanThotakura05 <83568543+SravanThotakura05@users.noreply.github.com>
2025-01-28 18:02:30 +0530

    Removed unwanted files and code

--- pubsubplus-connector-spark_3.x/src/main/java/com/solacecoe/connectors/spark/streaming/solace/exceptions/SolacePublishAbortException.java
+++ pubsubplus-connector-spark_3.x/src/main/java/com/solacecoe/connectors/spark/streaming/solace/exceptions/SolacePublishAbortException.java
@@ -1,10 +1,6 @@
 package com.solacecoe.connectors.spark.streaming.solace.exceptions;
 
 public class SolacePublishAbortException extends RuntimeException{
-    public SolacePublishAbortException(Throwable cause) {
-        super(cause);
-    }
-
     public SolacePublishAbortException(String cause) {
         super(cause);
     }
--- pubsubplus-connector-spark_3.x/src/main/java/com/solacecoe/connectors/spark/streaming/solace/exceptions/SolacePublishException.java
+++ pubsubplus-connector-spark_3.x/src/main/java/com/solacecoe/connectors/spark/streaming/solace/exceptions/SolacePublishException.java
@@ -1,10 +1,6 @@
 package com.solacecoe.connectors.spark.streaming.solace.exceptions;
 
 public class SolacePublishException extends RuntimeException{
-    public SolacePublishException(Throwable cause) {
-        super(cause);
-    }
-
     public SolacePublishException(String cause) {
         super(cause);
     }
