84a43df670009232134fd07adb0daaeb3bac17dc
SravanThotakura05 <83568543+SravanThotakura05@users.noreply.github.com>
2025-03-17 21:24:23 +0530

    Update SolaceSparkStreamingSourceIT.java

--- pubsubplus-connector-spark_3.x/src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingSourceIT.java
+++ pubsubplus-connector-spark_3.x/src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingSourceIT.java
@@ -37,7 +37,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class SolaceSparkStreamingSourceIT {
     private SempV2Api sempV2Api = null;
     private static final Long SHM_SIZE = (long) Math.pow(1024, 3);
-    private SolaceContainer solaceContainer = new SolaceContainer("solace/solace-pubsub-standard:latest").withCreateContainerCmdModifier(cmd ->{
+    private SolaceContainer solaceContainer = new SolaceContainer("solace/solace-pubsub-standard").withCreateContainerCmdModifier(cmd ->{
         cmd.getHostConfig()
                 .withShmSize(SHM_SIZE)
                 .withMemorySwap(-1L)
