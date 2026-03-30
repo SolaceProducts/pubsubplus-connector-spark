a4508b0f2f3fe46e98bf5f2935748ff129c0ab76
Sravan Thotakura <83568543+SravanThotakura05@users.noreply.github.com>
2026-03-30 10:20:25 +0530

    Debug

--- src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingOAuthIT.java
+++ src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingOAuthIT.java
@@ -133,7 +133,7 @@ public class SolaceSparkStreamingOAuthIT {
             );
 
             String logs = logResult.getStdout();
-
+            System.out.println(logs);
             // 4️⃣ Extract batchIds and numInputRows
             Matcher batchMatcher = batchPattern.matcher(logs);
             Matcher rowsMatcher = rowsPattern.matcher(logs);
