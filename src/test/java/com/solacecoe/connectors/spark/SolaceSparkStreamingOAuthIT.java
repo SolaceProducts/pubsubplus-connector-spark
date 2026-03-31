2c29ace6120ae8a9bb786464b37104c1a279ea16
Sravan Thotakura <83568543+SravanThotakura05@users.noreply.github.com>
2026-03-31 23:43:35 +0530

    Update SolaceSparkStreamingOAuthIT.java

--- src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingOAuthIT.java
+++ src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingOAuthIT.java
@@ -266,7 +266,7 @@ public class SolaceSparkStreamingOAuthIT {
     @Test
     @Order(5)
     void Should_ReadAccessTokenFromFile_And_ProcessData() throws TimeoutException, IOException, InterruptedException {
-        Path resources = Paths.get("tmp");
+        Path resources = Paths.get("src", "test", "resources");
 
         OAuthClient oAuthClient = new OAuthClient("https://localhost:7778/realms/solace/protocol/openid-connect/token", "solace", "solace-secret");
 
@@ -401,7 +401,7 @@ public class SolaceSparkStreamingOAuthIT {
 
     @Test
     void Should_Fail_When_AccessTokenIsInvalid() throws IOException, InterruptedException {
-        Path resources = Paths.get("tmp");
+        Path resources = Paths.get("src", "test", "resources");
 
         OAuthClient oAuthClient = new OAuthClient("https://localhost:7778/realms/solace/protocol/openid-connect/token", "solace", "solace-secret");
 
