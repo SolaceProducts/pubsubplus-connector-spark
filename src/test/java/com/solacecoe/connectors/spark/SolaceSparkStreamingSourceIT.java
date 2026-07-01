a69bfccc40bb3037b920ffa424197a0fd6158240
Sravan Thotakura <83568543+SravanThotakura05@users.noreply.github.com>
2026-07-01 08:07:30 +0530

    DATAGO-130445: property name changes, code clean up and doc update (#56)

--- src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingSourceIT.java
+++ src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingSourceIT.java
@@ -22,7 +22,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFilePermissions;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
