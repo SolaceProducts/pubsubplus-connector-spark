965f28e44e1e4170b81c7a5e3ba88d3be2ef6add
Sravan Thotakura <83568543+SravanThotakura05@users.noreply.github.com>
2026-08-25 17:38:41 +0530

    Update SolaceSparkStreamingSinkIT.java

--- src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingSinkIT.java
+++ src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingSinkIT.java
@@ -784,7 +784,6 @@ public class SolaceSparkStreamingSinkIT {
             assertEquals(3, msgVpnQueueTxFlowResponse.getData().size(), "Number of consumer flows should be 3");
         }
         Awaitility.await().atMost(90, TimeUnit.SECONDS).untilAsserted(() -> Assertions.assertTrue(count[0] >= 100));
-        System.out.println("Total records consumed from Solace " + count[0]);
         messageConsumer.stop();
         messageConsumer.close();
     }
