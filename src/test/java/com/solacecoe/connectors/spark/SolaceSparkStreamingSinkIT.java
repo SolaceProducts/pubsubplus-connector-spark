d0603dee9045424f7d01f44780b9a712d22ed1c0
Sravan Thotakura <83568543+SravanThotakura05@users.noreply.github.com>
2026-08-25 15:27:14 +0530

    Update SolaceSparkStreamingSinkIT.java

--- src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingSinkIT.java
+++ src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingSinkIT.java
@@ -600,7 +600,7 @@ public class SolaceSparkStreamingSinkIT {
             throw new RuntimeException(e);
         }
 
-        Awaitility.await().atMost(30, TimeUnit.SECONDS).untilAsserted(() -> Assertions.assertEquals(100, count[0]));
+        Awaitility.await().atMost(90, TimeUnit.SECONDS).untilAsserted(() -> Assertions.assertEquals(100, count[0]));
         messageConsumer.stop();
         messageConsumer.close();
     }
