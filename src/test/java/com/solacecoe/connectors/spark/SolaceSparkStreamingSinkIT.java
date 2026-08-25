ae845d38305fc928a404cafe6dc50a803f17a806
Sravan Thotakura <83568543+SravanThotakura05@users.noreply.github.com>
2026-08-25 17:36:07 +0530

    Update SolaceSparkStreamingSinkIT.java

--- src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingSinkIT.java
+++ src/test/java/com/solacecoe/connectors/spark/SolaceSparkStreamingSinkIT.java
@@ -764,6 +764,9 @@ public class SolaceSparkStreamingSinkIT {
                 @Override
                 public void onReceive(BytesXMLMessage bytesXMLMessage) {
                     count[0] = count[0] + 1;
+                    if(count[0] == 100) {
+                        System.out.println("Total records consumed from Solace " + count[0]);
+                    }
                 }
 
                 @Override
@@ -783,7 +786,7 @@ public class SolaceSparkStreamingSinkIT {
         if (msgVpnQueueTxFlowResponse.getData() != null && !msgVpnQueueTxFlowResponse.getData().isEmpty()) {
             assertEquals(3, msgVpnQueueTxFlowResponse.getData().size(), "Number of consumer flows should be 3");
         }
-        Awaitility.await().atMost(90, TimeUnit.SECONDS).untilAsserted(() -> Assertions.assertTrue(count[0] >= 100));
+        Awaitility.await().atMost(90, TimeUnit.SECONDS).untilAsserted(() -> Assertions.assertEquals(100, count[0]));
         messageConsumer.stop();
         messageConsumer.close();
     }
